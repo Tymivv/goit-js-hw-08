394324840a10f9464e2efba4ed159af974818987
Tymivv <43436409+Tymivv@users.noreply.github.com>
2021-11-14 21:48:44 +0000

    Deploying to gh-pages from @ Tymivv/goit-js-hw-08@43db1ac552c4c52027d3481431d6a5a0c58a5a7f 🚀

--- 01-gallery.057ae9cd.js
+++ 01-gallery.057ae9cd.js
@@ -8,4 +8,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"TfQ5":[function(require,module,exports) {
 "use strict";var e=a(require("simplelightbox"));require("simplelightbox/dist/simple-lightbox.min.css");var l=require("./gallery-items");function a(e){return e&&e.__esModule?e:{default:e}}const i=document.querySelector(".gallery"),r=l.galleryItems.map(({preview:e,original:l,description:a})=>`\n    <a class="gallery__item" href="${l}">\n    <img class="gallery__image" src="${e}" alt="${a}" title="${a}" />\n    </a>`).join("");i.insertAdjacentHTML("beforeend",r);var t=new e.default(".gallery a",{captionsData:"alt",captionDelay:250});
 },{"simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M","./gallery-items":"q5Rn"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/parcel-project-template/01-gallery.057ae9cd.js.map
+//# sourceMappingURL=/https:/github.com/Tymivv/goit-js-hw-08/01-gallery.057ae9cd.js.map
